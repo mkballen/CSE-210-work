04d990db6a1f36f5e4a7c7048bf6faa355659c1f
unknown <bal19034@byui.edu>
2024-03-02 23:29:20 -0700

    not finished action. will finish friday march 8.

---
+++ prove/Develop04/BreathingActivity.cs
@@ -0,0 +1,24 @@
+class BreathingActivity : Mindful{
+    private int countIn;
+    private int countOut;
+
+    public BreathingActivity(){
+        this.countIn = 0;
+        this.countOut = 0;
+    }
+    
+
+
+
+
+
+
+
+
+//     Console.WriteLine("Going to sleep for a second...");
+
+// Thread.Sleep(1000);
+
+// Console.WriteLine("I'm back!!");
+
+}
